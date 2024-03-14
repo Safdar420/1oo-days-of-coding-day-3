e41e69d245c54af6ec6a39554b94d0ceab864c2e
Safdar420 <160203243+Safdar420@users.noreply.github.com>
2024-03-14 10:04:50 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+// Q no 1
+var num0 = 5;
+var num1 = 3;
+console.log(num0 + num1);
+var num2 = 10;
+var num3 = 2;
+console.log(num2 - num3);
+var num4 = 4;
+var num5 = 2;
+console.log(num4 * num5);
+var num6 = 16;
+var num7 = 2;
+console.log(num6 / num7);
+// Q no 2
+console.log(5 + 3);
+console.log(10 - 2);
+console.log(4 * 2);
+console.log(16 / 2);
+// Q no 3
+var FavouriteNumber = 5;
+console.log("My Favouritenumber is ".concat(FavouriteNumber));
---
+++ main.ts
@@ -0,0 +1,29 @@
+// Q no 1
+
+let num0 :number = 5;
+let num1 :number = 3;
+console.log(num0 + num1);
+
+let num2 :number = 10;
+let num3 :number = 2;
+console.log(num2 - num3);
+
+let num4 :number = 4;
+let num5 :number = 2;
+console.log(num4 * num5);
+
+let num6 :number = 16;
+let num7 :number = 2;
+console.log(num6 / num7);
+
+// Q no 2
+
+console.log(5 + 3);
+console.log(10 - 2);
+console.log(4 * 2);
+console.log(16 / 2);
+
+// Q no 3
+
+let FavouriteNumber :number =5;
+console.log(`My Favourite number is ${FavouriteNumber}`);
